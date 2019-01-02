b3d042cad63c1081d78fdb771e8245ee9930e181
Alexey Siginur <siginur@gmail.com>
2019-01-02 12:24:57 +0200

    Initial Commit

---
+++ SMControl/SMControl.h
@@ -0,0 +1,19 @@
+//
+//  SMControl.h
+//  SMControl
+//
+//  Created by Alexey Siginur on 02/01/2019.
+//  Copyright © 2019 Alexey Siginur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMControl.
+FOUNDATION_EXPORT double SMControlVersionNumber;
+
+//! Project version string for SMControl.
+FOUNDATION_EXPORT const unsigned char SMControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMControl/PublicHeader.h>
+
+
