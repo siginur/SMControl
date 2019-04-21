280052997787c03f6bfa281d4c94549eb82ba38f
Alexey Siginur <siginur@gmail.com>
2019-04-21 11:47:10 +0300

    Add image and its scaling

--- SMControl/SMControl.swift
+++ SMControl/SMControl.swift
@@ -11,25 +11,62 @@ import UIKit
 /**
 Control structure
 **/
+@IBDesignable
 open class SMControl: UIControl {
 	
 	public typealias ActionBlock = (SMControl) -> ()
 	
 	private var actionsMap = [UInt : ActionBlock]()
+	private var imageView = UIImageView()
+	private var scaleImageConstraint: NSLayoutConstraint?
+	
+	@IBInspectable
+	public var image: UIImage? {
+		set {
+			imageView.image = newValue
+		}
+		get {
+			return imageView.image
+		}
+	}
+	
+	@IBInspectable
+	public var imageScale: CGFloat = 1 {
+		didSet {
+			if let constraint = scaleImageConstraint {
+				imageView.removeConstraint(constraint)
+			}
+			scaleImageConstraint = imageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: imageScale)
+			scaleImageConstraint?.isActive = true
+		}
+	}
 	
 	
 	// MARK: - Initializations
-
+	
 	public init() {
 		super.init(frame: CGRect.zero)
+		setup()
 	}
 	
 	override public init(frame: CGRect) {
 		super.init(frame: frame)
+		setup()
 	}
 	
 	required public init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
+		setup()
+	}
+	
+	private func setup() {
+		self.addSubview(imageView)
+		imageView.contentMode = .scaleAspectFit
+		imageView.isUserInteractionEnabled = true
+		imageView.translatesAutoresizingMaskIntoConstraints = false
+		imageView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+		imageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+		imageView.addConstraint(NSLayoutConstraint(item: imageView, attribute: .height, relatedBy: .equal, toItem: imageView, attribute: .width, multiplier: 1, constant: 0))
 	}
 	
 	
@@ -39,12 +76,17 @@ open class SMControl: UIControl {
 		actionsMap[event.rawValue] = block
 	}
 	
+	public func setImage(_ image: UIImage, scale: CGFloat) {
+		self.image = image
+		self.imageScale = scale
+	}
+	
 	private func execute(event: UIControl.Event) {
 		sendActions(for: event)
 		if let block = actionsMap[event.rawValue] {
 			block(self)
 		}
-
+		
 	}
 	
 	
@@ -56,14 +98,17 @@ open class SMControl: UIControl {
 		}
 		
 		execute(event: bounds.contains(location) ? .touchUpInside : .touchUpOutside)
+		imageView.alpha = 1
 	}
 	
 	override open func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
 		execute(event: .touchDown)
+		imageView.alpha = 0.7
 	}
 	
 	override open func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
 		execute(event: .touchCancel)
+		imageView.alpha = 1
 	}
-
+	
 }
